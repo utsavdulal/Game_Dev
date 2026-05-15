51aa85590f115072d28053bb1e3087c9c4f31174
utsavdulal <utsavdulal6@gmail.com>
2026-05-15 23:49:53 +0545

    background

--- Coursework/Assets/Scripts/InvestigationManager.cs
+++ Coursework/Assets/Scripts/InvestigationManager.cs
@@ -1,3 +1,35 @@
+// using UnityEngine;
+// using TMPro;
+
+// public class InvestigationManager : MonoBehaviour
+// {
+//     public TextMeshProUGUI resultText;
+
+//     public void ChooseMurder()
+//     {
+//         resultText.text =
+//             "Correct! The victim was murdered.";
+//     }
+
+//     public void ChooseSuicide()
+//     {
+//         resultText.text =
+//             "Incorrect conclusion.";
+//     }
+
+//     public void ChoosePoison()
+//     {
+//         resultText.text =
+//             "Incorrect conclusion.";
+//     }
+// }
+
+
+
+
+
+
+
 using UnityEngine;
 using TMPro;
 
@@ -5,21 +37,21 @@ public class InvestigationManager : MonoBehaviour
 {
     public TextMeshProUGUI resultText;
 
-    public void ChooseMurder()
+    public void ChoosePolitician()
     {
         resultText.text =
-            "Correct! The victim was murdered.";
+        "Correct.\n\nDaniel Hayes was murdered by a corrupt politician after discovering secret corruption files and illegal financial transactions.";
     }
 
-    public void ChooseSuicide()
+    public void ChooseFisherman()
     {
         resultText.text =
-            "Incorrect conclusion.";
+        "Incorrect.\n\nNo evidence connects the fisherman to the crime scene.";
     }
 
-    public void ChoosePoison()
+    public void ChooseBeggar()
     {
         resultText.text =
-            "Incorrect conclusion.";
+        "Incorrect.\n\nThe evidence suggests the murder was connected to political corruption.";
     }
 }
