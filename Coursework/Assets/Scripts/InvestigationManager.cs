ea0f2080836e4b4067b15fca97e62b578767eb98
utsavdulal <utsavdulal6@gmail.com>
2026-05-16 09:48:02 +0545

    someting

--- Coursework/Assets/Scripts/InvestigationManager.cs
+++ Coursework/Assets/Scripts/InvestigationManager.cs
@@ -40,18 +40,18 @@ public class InvestigationManager : MonoBehaviour
     public void ChoosePolitician()
     {
         resultText.text =
-        "Correct.\n\nDaniel Hayes was murdered by a corrupt politician after discovering secret corruption files and illegal financial transactions.";
+        "Correct.\nThe Journalist was murdered after uncovering political corruption.";
     }
 
     public void ChooseFisherman()
     {
         resultText.text =
-        "Incorrect.\n\nNo evidence connects the fisherman to the crime scene.";
+        "Incorrect.\nNo evidence connects the fisherman to the crime scene.";
     }
 
     public void ChooseBeggar()
     {
         resultText.text =
-        "Incorrect.\n\nThe evidence suggests the murder was connected to political corruption.";
+        "Incorrect.\nThe evidence suggests the murder was connected to political corruption.";
     }
 }
